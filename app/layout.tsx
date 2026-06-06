00bcbed62256ab943b797c6ec87b89723429ec54
charanatk <charanatk@gmail.com>
2026-06-06 03:24:08 -0400

    Added root layout

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "HR Portal",
+  description: "Startup HR Portal",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
